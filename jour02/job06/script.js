ad041053d82e1bd79f834ef0fef998aecf752a36
aurelien-gallea <aurelien.gallea@laplateforme.io>
2023-01-19 00:11:22 +0100

    amelioration de la logique d'utilisation du code

--- jour02/job06/script.js
+++ jour02/job06/script.js
@@ -11,21 +11,18 @@ body.addEventListener('keyup', (e) => {
     let code = e.key;
     checkCode.push(code);
     
-    if (checkCode.length == 10) {
-        for (let i = 0; i < konamiCode.length; i++) {
-            if (konamiCode[i] === checkCode[i]) {
-                count++;
-            } else {
-                break;
-            }   
-        }
+    for (let i = 0; i < checkCode.length; i++) {
+        if (konamiCode[i] === checkCode[i]) {
+            count++;
+        } else {
+            checkCode= [];
+            break;
+        }   
+    }    
+    if (checkCode.length == 10) {       
         if(count === checkCode.length) {
             body.style.background = "linear-gradient(0.25turn, #275FA0,#FFCA4B)";
-        }
-        return checkCode = [];
-    } 
-    return count = 0;
-    
-    
-    
+        }    
+    }
+    count = 0;
 });
