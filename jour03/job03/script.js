576a2a313b9e3da8a8cb2aaba731fe297ec0e810
aurelien-gallea <aurelien.gallea@laplateforme.io>
2023-01-19 21:52:46 +0100

    ajout du job3 (non termine)

--- jour03/job03/script.js
+++ jour03/job03/script.js
@@ -13,57 +13,178 @@ function randomize(tab) {
 }
 console.log(randomPos);
 // on place tout sur la tableau de jeu aléatoirement
-$("#box").append($(`.1`).append($(`#img${randomPos[0]}`)));
-$("#box").append($(`.2`).append($(`#img${randomPos[1]}`)));
-$("#box").append($(`.3`).append($(`#img${randomPos[2]}`)));
-$("#box").append($(`.4`).append($(`#img${randomPos[3]}`)));
-$("#box").append($(`.5`).append($(`#img${randomPos[4]}`)));
-$("#box").append($(`.6`).append($(`#img${randomPos[5]}`)));
-$("#box").append($(`.7`).append($(`#img${randomPos[6]}`)));
-$("#box").append($(`.8`).append($(`#img${randomPos[7]}`)));
-$("#box").append($(`.9`));
+$(`.1`).append($(`#img${randomPos[0]}`));
+$(`.2`).append($(`#img${randomPos[1]}`));
+$(`.3`).append($(`#img${randomPos[2]}`));
+$(`.4`).append($(`#img${randomPos[3]}`));
+$(`.5`).append($(`#img${randomPos[4]}`));
+$(`.6`).append($(`#img${randomPos[5]}`));
+$(`.7`).append($(`#img${randomPos[6]}`));
+$(`.8`).append($(`#img${randomPos[7]}`));
+// $("#box").append($(`.9`)).append($('#img9'));
 
-
-let emptyCase = 9;
 function checkMove() {
+  let empty = 9;
   // div9  --------------------------------
   $(".6").click(() => {
-    if ($(.9).text() == "") {
+    if (empty == 9 && empty != 6) {
       $(".9").append($(`#img${randomPos[5]}`));
-      emptyCase = 6;
+      $(".6").toggleClass("empty");
+      return empty = 6;
     }
-});
-console.log(emptyCase);
+  });
 
   $(".8").click(() => {
-
-    if ($(.9).hasClass('empty')) {
+    if (empty == 9 && empty !=8) {
       $(".9").append($(`#img${randomPos[7]}`));
-      
+      return empty = 8;
     }
-    return emptyCase = 8;
-    
   });
   // div8 ----------------------------------
   $(".5").click(() => {
-    if ($(.8).text() == "") {
+    if (empty == 8) {
       $(".8").append($(`#img${randomPos[4]}`));
-      return emptyCase = 5;
+      return empty = 5;
     }
   });
   $(".7").click(() => {
-    if ($(.8).text() == "") {
+    if (empty == 8) {
       $(".8").append($(`#img${randomPos[6]}`));
-      return emptyCase = 7;
+      return empty = 7;
     }
   });
   $(".9").click(() => {
-    if ($(.8).text() == "") {
+    if (empty == 8) {
       $(".8").append($(`#img${randomPos[8]}`));
-      return emptyCase = 9;
+      return empty = 9;
+    }
+  });
+  // div7 ----------------------------------
+  $(".8").click(() => {
+    if (empty == 7 && empty !=8) {
+      $(".7").append($(`#img${randomPos[7]}`));
+      return empty = 8;
+    }
+  });
+  $(".4").click(() => {
+    console.log($('.8'));
+    if (empty == 7) {
+      $(".7").append($(`#img${randomPos[3]}`));
+      return empty = 4;
+    }
+  });
+  // div6 ---------------------------------
+  $(".3").click(() => {
+    if (empty == 6) {
+      $(".6").append($(`#img${randomPos[2]}`));
+      return empty = 3;
+    }
+  });
+  $(".5").click(() => {
+    if (empty == 6) {
+      $(".6").append($(`#img${randomPos[4]}`));
+      return empty = 5;
+    }
+  });
+  $(".9").click(() => {
+    if (empty == 6) {
+      $(".6").append($(`#img${randomPos[8]}`));
+      return empty = 9;
+    }
+  });
+  // div5 -------------------------------
+  $(".2").click(() => {
+    if (empty == 5) {
+      $(".5").append($(`#img${randomPos[1]}`));
+      return empty = 2;
+    }
+  });
+  $(".4").click(() => {
+    if (empty == 5) {
+      $(".5").append($(`#img${randomPos[3]}`));
+      return empty = 4;
+    }
+  });
+  $(".6").click(() => {
+    if (empty == 5 && empty != 6) {
+      $(".5").append($(`#img${randomPos[5]}`));
+      $(".6").toggleClass("empty");
+      return empty = 6;
+    }
+  });
+  $(".8").click(() => {
+    if (empty == 5 && empty !=8) {
+      $(".5").append($(`#img${randomPos[7]}`));
+      return empty = 8;
+    }
+  });
+  // div4---------------------------------1/5/7
+  $(".1").click(() => {
+    if (empty == 4) {
+      $(".4").append($(`#img${randomPos[0]}`));
+      return empty = 1;
+    }
+  });
+  $(".5").click(() => {
+    if (empty == 4) {
+      $(".4").append($(`#img${randomPos[4]}`));
+      return empty = 5;
+    }
+  });
+  $(".7").click(() => {
+    if (empty == 4) {
+      $(".4").append($(`#img${randomPos[6]}`));
+      return empty = 7;
+    }
+  });
+  // div3-------------------------------2/6
+  $(".2").click(() => {
+    if (empty == 3) {
+      $(".3").append($(`#img${randomPos[1]}`));
+      return empty = 2;
+    }
+  });
+  $(".6").click(() => {
+    if (empty == 3 && empty != 6) {
+      $(".3").append($(`#img${randomPos[5]}`));
+      $(".6").toggleClass("empty");
+      return empty = 6;
+    }
+  });
+  // div2-----------------------------1/3/5
+  $(".1").click(() => {
+    if (empty == 2) {
+      $(".2").append($(`#img${randomPos[0]}`));
+      return empty = 1;
+    }
+  });
+  $(".3").click(() => {
+    if (empty == 2) {
+      $(".2").append($(`#img${randomPos[2]}`));
+      return empty = 3;
+    }
+  });
+  $(".5").click(() => {
+    if (empty == 2) {
+      $(".2").append($(`#img${randomPos[4]}`));
+      return empty = 5;
+    }
+  });
+  // div1 ------------------------------2/4
+  $(".2").click(() => {
+    if (empty == 1) {
+      $(".1").append($(`#img${randomPos[1]}`));
+      return empty = 2;
+    }
+  });
+  $(".4").click(() => {
+    if (empty == 1) {
+      $(".1").append($(`#img${randomPos[3]}`));
+      return empty = 4;
     }
   });
 }
 checkMove();
 
-console.log($(0.8).click());
+
+
